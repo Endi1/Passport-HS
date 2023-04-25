03b1cda83d9dedeff6a4b1b0038293b80d21b0bc
Endi Sukaj <endisukaj@gmail.com>
2023-04-26 00:03:54 +0200

    Add Auth functions

--- Passport/Auth.hs
+++ Passport/Auth.hs
@@ -1,4 +1,95 @@
-module Passport.Auth (main) where
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-main :: IO ()
-main = print "Hello world"
+module Passport.Auth () where
+
+import Control.Monad.Trans.Except (ExceptT (ExceptT), withExceptT)
+import Data.Aeson (Value (String))
+import Network.HTTP.Conduit (newManager, tlsManagerSettings)
+import Network.HTTP.Types (status302, status500)
+import Network.OAuth.OAuth2
+  ( ExchangeToken (ExchangeToken),
+    OAuth2Token (accessToken),
+    authGetJSON,
+    fetchAccessToken,
+  )
+import Passport.Config (Auth0User (email), auth0, auth0UserInfoUri, authorizeUrl)
+import Passport.Utils (byteStringLazyToText, excepttToActionM, oauth2ErrorToText, paramValue, textToUri, uriToText)
+import RIO
+  ( Text,
+    liftIO,
+    void,
+  )
+import RIO.Map qualified as Map
+import RIO.Text (pack)
+import RIO.Text.Lazy qualified as TL
+import System.Environment (getEnv)
+import Web.JWT
+  ( ClaimsMap (ClaimsMap),
+    JWTClaimsSet (iss, unregisteredClaims),
+    encodeSigned,
+    hmacSecret,
+    stringOrURI,
+  )
+import Web.Scotty (ActionM, params, redirect, setHeader, status)
+import Web.Scotty.Cookie (deleteCookie, setSimpleCookie)
+
+loginH :: ActionM ()
+loginH = do
+  oauthRedirectUri <- liftIO $ getEnv "OAUTH_REDIRECT_URI"
+  clientID <- liftIO $ getEnv "OAUTH_CLIENT_ID"
+  clientSecret <- liftIO $ getEnv "OAUTH_CLIENT_SECRET"
+
+  case authorizeUrl (pack clientID) (pack clientSecret) (pack oauthRedirectUri) of
+    Nothing -> status status500
+    Just u -> do
+      setHeader "Location" $ uriToText u
+      status status302
+
+callbackH :: (Text -> IO a) -> ActionM ()
+callbackH authCallback = do
+  oauthRedirectUri <- liftIO $ getEnv "OAUTH_REDIRECT_URI"
+  pas <- params
+  case textToUri $ pack oauthRedirectUri of
+    Nothing -> status status500
+    Just u -> do
+      userEmail <- excepttToActionM $ do
+        void $ ExceptT $ pure $ paramValue "state" pas
+        codeP <- ExceptT $ pure $ paramValue "code" pas
+
+        mgr <- liftIO $ newManager tlsManagerSettings
+
+        -- Exchange authorization code for Access Token
+        -- 'oauth2ErrorToText' turns (OAuth2 error) to Text which is the default way
+        -- Scotty represents error message
+        clientID <- liftIO $ pack <$> getEnv "OAUTH_CLIENT_ID"
+        clientSecret <- liftIO $ pack <$> getEnv "OAUTH_CLIENT_SECRET"
+        let code = ExchangeToken $ TL.toStrict codeP
+        tokenResp <- withExceptT oauth2ErrorToText (fetchAccessToken mgr (auth0 clientID clientSecret u) code)
+
+        -- Call API to resource server with Access Token being authentication code.
+        -- 'byteStringLazyToText' exists for similar reason as 'oauth2ErrorToText'
+        let at = accessToken tokenResp
+        user <- withExceptT byteStringLazyToText (authGetJSON mgr at auth0UserInfoUri)
+
+        _ <- liftIO $ authCallback (TL.toStrict $ email user)
+        return (email user)
+
+      setSimpleCookie "auth-token" $ TL.toStrict $ generateToken userEmail
+      redirect "/"
+
+generateToken :: TL.Text -> TL.Text
+generateToken userEmail =
+  TL.fromStrict $
+    encodeSigned
+      (hmacSecret "secret-key")
+      mempty
+      mempty -- mempty returns a default JWTClaimsSet
+        { iss = stringOrURI "RecipeManager",
+          unregisteredClaims = ClaimsMap $ Map.fromList [("userEmail", String $ TL.toStrict userEmail)]
+        }
+
+signOutH :: ActionM ()
+signOutH = do
+  deleteCookie "auth-token"
+  redirect "/"
--- Passport/Utils.hs
+++ Passport/Utils.hs
@@ -1,11 +1,52 @@
-module Passport.Utils (textToUri) where
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedStrings #-}
 
+module Passport.Utils (textToUri, byteStringLazyToText, excepttToActionM, oauth2ErrorToText, paramValue, uriToText) where
+
+import Control.Exception (throw)
+import Control.Monad.Trans.Except (ExceptT, runExceptT)
+import Data.ByteString.Lazy.Char8 qualified as BSL
+import Network.OAuth.OAuth2 (OAuth2Error)
+import Network.OAuth.OAuth2.TokenRequest (Errors)
 import Passport.Types (OAuthRedirectURI)
 import RIO
-import URI.ByteString (parseURI, strictURIParserOptions)
+import RIO.Text.Lazy qualified as TL
+import URI.ByteString (URI, parseURI, serializeURIRef', strictURIParserOptions)
+import Web.Scotty (ActionM, Param, raise)
 
 textToUri :: Text -> Maybe OAuthRedirectURI
 textToUri uriText =
   case parseURI strictURIParserOptions $ encodeUtf8 uriText of
     Left _ -> Nothing
     Right u -> Just u
+
+uriToText :: URI -> TL.Text
+uriToText u = case decodeUtf8' $ serializeURIRef' u of
+  Left decodeErr -> throw decodeErr
+  Right decodedURI -> TL.fromStrict decodedURI
+
+byteStringLazyToText :: BSL.ByteString -> TL.Text
+byteStringLazyToText = TL.pack . BSL.unpack
+
+excepttToActionM :: ExceptT TL.Text IO a -> ActionM a
+excepttToActionM e = do
+  result <- liftIO $ runExceptT e
+  either raise pure result
+
+oauth2ErrorToText :: OAuth2Error Errors -> TL.Text
+oauth2ErrorToText e = TL.pack $ "Unable fetch access token. error detail: " ++ show e
+
+paramValue ::
+  -- | Parameter key
+  TL.Text ->
+  -- | All parameters
+  [Param] ->
+  Either TL.Text TL.Text
+paramValue key params =
+  if null val
+    then Left ("No value found for param: " <> key)
+    else Right (head val)
+  where
+    val = snd <$> filter (hasParam key) params
+    hasParam :: TL.Text -> Param -> Bool
+    hasParam t = (== t) . fst
